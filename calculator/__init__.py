8f061b037d8574eaafb4bf5cb3ad60a46125ae3c
sudeepreddy143 <sudeeppanyam@gmail.com>
2025-02-11 00:05:02 +0300

    78% yet to pass 100% coverage

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,23 +1,22 @@
-from calculator.calculation import Calculation
 from calculator.operations import add, subtract, multiply, divide
 
 class Calculator:
     @staticmethod
     def add(num1, num2):
-        calculation = Calculation(num1, num2, add)
+        calculation = calculation(num1, num2, add)
         return calculation.get_result()
 
     @staticmethod
     def subtract(num1, num2):
-        calculation = Calculation(num1, num2, subtract)
+        calculation = calculation(num1, num2, subtract)
         return calculation.get_result()
 
     @staticmethod
     def multiply(num1, num2):
-        calculation = Calculation(num1, num2, multiply)
+        calculation = calculation(num1, num2, multiply)
         return calculation.get_result()
 
     @staticmethod
     def divide(num1, num2):
-        calculation = Calculation(num1, num2, divide)
+        calculation = calculation(num1, num2, divide)
         return calculation.get_result()
